27cca84f962c1cb808d5392e3df0937bc4a27569
luisantoniococa <luicocurd@gmail.com>
2020-03-07 13:22:16 -0600

    added the circles opacity

--- static/js/logic.js
+++ static/js/logic.js
@@ -86,44 +86,44 @@ var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_we
 
 
 
-    var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-      maxZoom: 18,
-      id: "mapbox.streets",
-      accessToken: API_KEY
-    });
-  
-    var darkmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-      maxZoom: 18,
-      id: "mapbox.dark",
-      accessToken: API_KEY
-    });
+var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets",
+  accessToken: API_KEY
+});
 
-    var layers = {
-      zeroToOne: new L.LayerGroup(),
-      oneToTwo: new L.LayerGroup(),
-      twoToThree: new L.LayerGroup(),
-      threeToFour: new L.LayerGroup(),
-      fourToFive: new L.LayerGroup(),
-      fivePlus: new L.LayerGroup()
-    }
+var darkmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.dark",
+  accessToken: API_KEY
+});
 
-    var myMap = L.map("map", {
-      center: [
-        37.09, -95.71
-      ],
-      zoom: 5,
-      layers: [
-        layers.zeroToOne,
-        layers.oneToTwo,
-        layers.twoToThree,
-        layers.threeToFour,
-        layers.fourToFive,
-        layers.fivePlus
-      ]
-    });
-  streetmap.addTo(myMap);
+var layers = {
+  zeroToOne: new L.LayerGroup(),
+  oneToTwo: new L.LayerGroup(),
+  twoToThree: new L.LayerGroup(),
+  threeToFour: new L.LayerGroup(),
+  fourToFive: new L.LayerGroup(),
+  fivePlus: new L.LayerGroup()
+}
+
+var myMap = L.map("map", {
+  center: [
+    37.09, -95.71
+  ],
+  zoom: 5,
+  layers: [
+    layers.zeroToOne,
+    layers.oneToTwo,
+    layers.twoToThree,
+    layers.threeToFour,
+    layers.fourToFive,
+    layers.fivePlus
+  ]
+});
+streetmap.addTo(myMap);
 
 var overlays = {
   "0-1":layers.zeroToOne,
@@ -136,18 +136,57 @@ var overlays = {
 }
 
 L.control.layers(null, overlays).addTo(myMap);
-
 var info = L.control({
   position: "bottomright"
 });
 
-info.onAdd = function() {
-  var div = L.DomUtil.create("div", "legend");
+var colors= ["#fef0d9","#fdd49e",'#fdbb84','#fc8d59','#e34a33','#b30000']
+function getColor(d) {
+  return d === '0-1'  ? "#de2d26" :
+         d === '1-2'  ? "#377eb8" :
+         d === '2-3' ? "#4daf4a" :
+         d === '3-4' ? "#984ea3" :
+         d === '4-5' ? "#984ea3" :
+                      "#ff7f00";
+}
+
+function style(feature) {
+  return {
+      weight: 1.5,
+      opacity: 1,
+      fillOpacity: 1,
+      radius: 6,
+      fillColor: getColor(),
+      color: "grey"
+
+  };
+}
+var legend = L.control({position: 'bottomleft', background: 'white'});
+  legend.onAdd = function () {
+
+  var div = L.DomUtil.create('div', 'info legend');
+  labels = ['<strong>Categories</strong>'],
+  categories = ["zeroToOne",
+                "oneToTwo",
+                "twoToThree",
+                "threeToFour",
+                "fourToFive",
+                "fivePlus"]
+  
+
+
+  for (var i = 0; i < categories.length; i++) {
+
+          div.innerHTML += 
+          labels.push(
+            '<i style="background:' + getColor(categories[i] + 1) + '"></i> ' +
+            (categories[i] ? categories[i] : '+'));
+
+      }
+      div.innerHTML = labels.join('<br>');
   return div;
-};
-
-info.addTo(myMap);
-
+  };
+  legend.addTo(myMap);
 d3.json(queryUrl, function(response) {
     
   // Create a new marker cluster group
@@ -156,27 +195,38 @@ d3.json(queryUrl, function(response) {
   // Loop through data
       var location = response.features;
   //   console.log(location)
+
   location.forEach(element => {
       console.log(`this is the element ${element.geometry.coordinates[1]}`)
       
       var color = "";
-      if (element.properties.mag > 5) {
+      var magitude = element.properties.mag;
+      var layerName = ""
+
+      if (magitude > 5) {
         color = "#b30000";
+        layerName = "fivePlus"
+
       }
-      else if (element.properties.mag > 4) {
+      else if (magitude > 4) {
         color = "#e34a33";
+        layerName = "fourToFive"
       }
-      else if (element.properties.mag > 3) {
+      else if (magitude > 3) {
         color = "#fc8d59";
+        layerName = "threeToFour"
       }
-      else if (element.properties.mag > 2) {
+      else if (magitude > 2) {
         color = "#fdbb84";
+        layerName = "twoToThree"
       }
-      else if (element.properties.mag > 1) {
+      else if (magitude > 1) {
         color = "#fdd49e";
+        layerName = "oneToTwo"
       }
       else {
         color = "#fef0d9";
+        layerName = "zeroToOne"
       }
     
     //   if (location) {
@@ -185,24 +235,53 @@ d3.json(queryUrl, function(response) {
     //   markers.addLayer(L.marker([parseFloat(element.geometry.coordinates[1]), parseFloat(element.geometry.coordinates[0])])
     //     .bindPopup(element.properties.mag));
     // }
-
-    L.circle([parseFloat(element.geometry.coordinates[1]), parseFloat(element.geometry.coordinates[0])], {
+    
+    var circles = L.circle([parseFloat(element.geometry.coordinates[1]), parseFloat(element.geometry.coordinates[0])], {
       fillOpacity: 0.85,
       color: color,
       fillColor: color,
       // Adjust radius
       radius: parseFloat(element.properties.mag) * 15000
       // radius: 1000
-    }).addTo(myMap);
-      
+    })
+  // }).addTo(myMap);
+    circles.addTo(layers[layerName])
 
 
-    
   });
     // Check for location property
+    // var legend = L.control({ position: "bottomright" });
+    // legend.onAdd = function() {
+    //   var div = L.DomUtil.create("div", "info legend");
+    //   var limits = 6;
+    //   var colors = location.color;
+    //   var labels = [];
+  
+    //   // Add min & max
+    //   var legendInfo = "<h1></h1>" +
+    //     "<div class=\"labels\">" +
+    //       "<div class=\"min\">" + limits[0] + "</div>" +
+    //       "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
+    //     "</div>";
+  
+    //   div.innerHTML = legendInfo;
+  
+    //   limits.forEach(function(limit, index) {
+    //     labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
+    //   });
+  
+    //   div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+    //   return div;
+    // };
+  
+    // // Adding legend to the map
+    // legend.addTo(myMap);
+
+
 
 
   // }
+
 
   // Add our marker cluster layer to the map
   // myMap.addLayer(markers);
--- static/js/logic1.js
+++ static/js/logic1.js
@@ -1,222 +1,115 @@
-var myMap = L.map("map", {
-    center: [37.09, -95.71],
-    zoom: 5
-  });
-  
-  // Adding tile layer to the map
-  L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
-    id: "mapbox.streets",
-    accessToken: API_KEY
-  }).addTo(myMap);
-  
-  // Store API query variables
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
-  
-  // Assemble API query URL
-  var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
-  
-  // Grab the data with d3
-  d3.json(url, function(response) {
-    
-    // Create a new marker cluster group
-    var markers = L.markerClusterGroup();
-  
-    // Loop through data
-    // for (var i = 0; i < response.features.length; i++) {
-        
-      // Set the data location property to a variable
-    //   var location = response[i].features.geometry;
-        var location = response.features;
-    //   console.log(location)
-    location.forEach(element => {
-        console.log(`this is the element ${element.geometry.coordinates[1]}`)
-      if (location) {
-  
-        // Add a new marker to the cluster group and bind a pop-up
-        markers.addLayer(L.marker([parseFloat(element.geometry.coordinates[1]), parseFloat(element.geometry.coordinates[0])])
-          .bindPopup(element.properties.mag));
-      }
-        
-    });
-      // Check for location property
-
-  
-    // }
-  
-    // Add our marker cluster layer to the map
-    myMap.addLayer(markers);
-  
-  });
-
-  
-
-  var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"http://mapbox.com\">Mapbox</a>",
+var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
   maxZoom: 18,
-  id: "mapbox.light",
+  id: "mapbox.streets",
   accessToken: API_KEY
 });
 
-// Initialize all of the LayerGroups we'll be using
-var layers = {
-  COMING_SOON: new L.LayerGroup(),
-  EMPTY: new L.LayerGroup(),
-  LOW: new L.LayerGroup(),
-  NORMAL: new L.LayerGroup(),
-  OUT_OF_ORDER: new L.LayerGroup()
-};
-
-// Create the map with our layers
-var map = L.map("map-id", {
-  center: [40.73, -74.0059],
-  zoom: 12,
-  layers: [
-    layers.COMING_SOON,
-    layers.EMPTY,
-    layers.LOW,
-    layers.NORMAL,
-    layers.OUT_OF_ORDER
-  ]
+var darkmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.dark",
+  accessToken: API_KEY
 });
 
-// Add our 'lightmap' tile layer to the map
-lightmap.addTo(map);
+var layers = {
+  zeroToOne: new L.LayerGroup(),
+  oneToTwo: new L.LayerGroup(),
+  twoToThree: new L.LayerGroup(),
+  threeToFour: new L.LayerGroup(),
+  fourToFive: new L.LayerGroup(),
+  fivePlus: new L.LayerGroup()
+}
 
-// Create an overlays object to add to the layer control
+var myMap = L.map("map", {
+  center: [
+    37.09, -95.71
+  ],
+  zoom: 5,
+  layers: [
+    layers.zeroToOne,
+    layers.oneToTwo,
+    layers.twoToThree,
+    layers.threeToFour,
+    layers.fourToFive,
+    layers.fivePlus
+  ]
+});
+streetmap.addTo(myMap);
+
 var overlays = {
-  "Coming Soon": layers.COMING_SOON,
-  "Empty Stations": layers.EMPTY,
-  "Low Stations": layers.LOW,
-  "Healthy Stations": layers.NORMAL,
-  "Out of Order": layers.OUT_OF_ORDER
-};
+  "0-1":layers.zeroToOne,
+  "1-2":layers.oneToTwo,
+  "2-3":layers.twoToThree,
+  "3-4":layers.threeToFour,
+  "4-5":layers.fourToFive,
+  "5+":layers.fivePlus
+  
+}
 
-// Create a control for our layers, add our overlay layers to it
-L.control.layers(null, overlays).addTo(map);
-
-// Create a legend to display information about our map
+L.control.layers(null, overlays).addTo(myMap);
 var info = L.control({
   position: "bottomright"
 });
 
-// When the layer control is added, insert a div with the class of "legend"
-info.onAdd = function() {
-  var div = L.DomUtil.create("div", "legend");
-  return div;
-};
-// Add the info legend to the map
-info.addTo(map);
 
-// Initialize an object containing icons for each layer group
-var icons = {
-  COMING_SOON: L.ExtraMarkers.icon({
-    icon: "ion-settings",
-    iconColor: "white",
-    markerColor: "yellow",
-    shape: "star"
-  }),
-  EMPTY: L.ExtraMarkers.icon({
-    icon: "ion-android-bicycle",
-    iconColor: "white",
-    markerColor: "red",
-    shape: "circle"
-  }),
-  OUT_OF_ORDER: L.ExtraMarkers.icon({
-    icon: "ion-minus-circled",
-    iconColor: "white",
-    markerColor: "blue-dark",
-    shape: "penta"
-  }),
-  LOW: L.ExtraMarkers.icon({
-    icon: "ion-android-bicycle",
-    iconColor: "white",
-    markerColor: "orange",
-    shape: "circle"
-  }),
-  NORMAL: L.ExtraMarkers.icon({
-    icon: "ion-android-bicycle",
-    iconColor: "white",
-    markerColor: "green",
-    shape: "circle"
-  })
-};
+d3.json(queryUrl, function(response) {
+    
+    // Create a new marker cluster group
 
-// Perform an API call to the Citi Bike Station Information endpoint
-d3.json("https://gbfs.citibikenyc.com/gbfs/en/station_information.json", function(infoRes) {
+    // Loop through data
+        var location = response.features;
 
-  // When the first API call is complete, perform another call to the Citi Bike Station Status endpoint
-  d3.json("https://gbfs.citibikenyc.com/gbfs/en/station_status.json", function(statusRes) {
-    var updatedAt = infoRes.last_updated;
-    var stationStatus = statusRes.data.stations;
-    var stationInfo = infoRes.data.stations;
+  
+    location.forEach(element => {
+        console.log(`this is the element ${element.geometry.coordinates[1]}`)
+        
+        var color = "";
+        var magitude = element.properties.mag;
+        var layerName = ""
+  
+        if (magitude > 5) {
+          color = "#b30000";
+          layerName = "fivePlus"
+  
+        }
+        else if (magitude > 4) {
+          color = "#e34a33";
+          layerName = "fourToFive"
+        }
+        else if (magitude > 3) {
+          color = "#fc8d59";
+          layerName = "threeToFour"
+        }
+        else if (magitude > 2) {
+          color = "#fdbb84";
+          layerName = "twoToThree"
+        }
+        else if (magitude > 1) {
+          color = "#fdd49e";
+          layerName = "oneToTwo"
+        }
+        else {
+          color = "#fef0d9";
+          layerName = "zeroToOne"
+        }
 
-    // Create an object to keep of the number of markers in each layer
-    var stationCount = {
-      COMING_SOON: 0,
-      EMPTY: 0,
-      LOW: 0,
-      NORMAL: 0,
-      OUT_OF_ORDER: 0
-    };
+      
+      var circles = L.circle([parseFloat(element.geometry.coordinates[1]), parseFloat(element.geometry.coordinates[0])], {
+        fillOpacity: 0.85,
+        color: color,
+        fillColor: color,
+        // Adjust radius
+        radius: parseFloat(element.properties.mag) * 15000
+        // radius: 1000
+      })
+    // }).addTo(myMap);
+      circles.addTo(layers[layerName])
+  
+  
+    });
 
-    // Initialize a stationStatusCode, which will be used as a key to access the appropriate layers, icons, and station count for layer group
-    var stationStatusCode;
-
-    // Loop through the stations (they're the same size and have partially matching data)
-    for (var i = 0; i < stationInfo.length; i++) {
-
-      // Create a new station object with properties of both station objects
-      var station = Object.assign({}, stationInfo[i], stationStatus[i]);
-      // If a station is listed but not installed, it's coming soon
-      if (!station.is_installed) {
-        stationStatusCode = "COMING_SOON";
-      }
-      // If a station has no bikes available, it's empty
-      else if (!station.num_bikes_available) {
-        stationStatusCode = "EMPTY";
-      }
-      // If a station is installed but isn't renting, it's out of order
-      else if (station.is_installed && !station.is_renting) {
-        stationStatusCode = "OUT_OF_ORDER";
-      }
-      // If a station has less than 5 bikes, it's status is low
-      else if (station.num_bikes_available < 5) {
-        stationStatusCode = "LOW";
-      }
-      // Otherwise the station is normal
-      else {
-        stationStatusCode = "NORMAL";
-      }
-
-      // Update the station count
-      stationCount[stationStatusCode]++;
-      // Create a new marker with the appropriate icon and coordinates
-      var newMarker = L.marker([station.lat, station.lon], {
-        icon: icons[stationStatusCode]
-      });
-
-      // Add the new marker to the appropriate layer
-      newMarker.addTo(layers[stationStatusCode]);
-
-      // Bind a popup to the marker that will  display on click. This will be rendered as HTML
-      newMarker.bindPopup(station.name + "<br> Capacity: " + station.capacity + "<br>" + station.num_bikes_available + " Bikes Available");
-    }
-
-    // Call the updateLegend function, which will... update the legend!
-    updateLegend(updatedAt, stationCount);
+  
   });
-});
-
-// Update the legend's innerHTML with the last updated time and station count
-function updateLegend(time, stationCount) {
-  document.querySelector(".legend").innerHTML = [
-    "<p>Updated: " + moment.unix(time).format("h:mm:ss A") + "</p>",
-    "<p class='out-of-order'>Out of Order Stations: " + stationCount.OUT_OF_ORDER + "</p>",
-    "<p class='coming-soon'>Stations Coming Soon: " + stationCount.COMING_SOON + "</p>",
-    "<p class='empty'>Empty Stations: " + stationCount.EMPTY + "</p>",
-    "<p class='low'>Low Stations: " + stationCount.LOW + "</p>",
-    "<p class='healthy'>Healthy Stations: " + stationCount.NORMAL + "</p>"
-  ].join("");
-}
